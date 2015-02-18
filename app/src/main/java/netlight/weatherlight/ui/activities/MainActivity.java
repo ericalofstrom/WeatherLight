8ac1d49c1cfd9acd3940c0d1667d437b9b8f85b2
olemaga <ole.magnus.aker@gmail.com>
2015-02-18 16:48:51 +0100

    added detail views

--- app/src/main/java/netlight/weatherlight/ui/activities/MainActivity.java
+++ app/src/main/java/netlight/weatherlight/ui/activities/MainActivity.java
@@ -1,7 +1,8 @@
 package netlight.weatherlight.ui.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 
 import netlight.weatherlight.R;
 import netlight.weatherlight.model.Office;
@@ -10,26 +11,41 @@ import netlight.weatherlight.ui.fragments.OfficeDetailFragment;
 import netlight.weatherlight.ui.fragments.OfficeListFragment;
 
 
-public class MainActivity extends ActionBarActivity implements OfficeClickedListener {
+public class MainActivity extends ActionBarActivity implements OfficeClickedListener, FragmentManager.OnBackStackChangedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
+            getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new OfficeListFragment())
                     .commit();
         }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.addOnBackStackChangedListener(this);
     }
 
     @Override
     public void officeClicked(Office office) {
-        getFragmentManager().beginTransaction()
-                .add(R.id.container, OfficeDetailFragment.newInstance(office.getCity() + ", " + office.getCountry()))
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, OfficeDetailFragment.newInstance(office.getCity() + ", " + office.getCountry()))
                 .addToBackStack(null)
                 .commit();
     }
 
+    @Override
+    public void onBackPressed() {
+        getSupportFragmentManager().popBackStack();
+    }
 
+    @Override
+    public void onBackStackChanged() {
+        int backstackCount = getSupportFragmentManager().getBackStackEntryCount();
+        if (backstackCount > 0) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        } else {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+    }
 }
