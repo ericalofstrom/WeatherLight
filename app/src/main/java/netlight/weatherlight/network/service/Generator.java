fa3f5699d8f55e27eca7ad57e6470ead034dcac2
ag12 <amirg12@hotmail.no>
2015-02-18 17:36:49 +0100

    Fix my bug

--- app/src/main/java/netlight/weatherlight/network/service/Generator.java
+++ app/src/main/java/netlight/weatherlight/network/service/Generator.java
@@ -1,11 +1,13 @@
 package netlight.weatherlight.network.service;
 
+import com.google.gson.Gson;
 import com.squareup.okhttp.OkHttpClient;
 
 import netlight.weatherlight.BuildConfig;
 import retrofit.RestAdapter;
 import retrofit.android.AndroidLog;
 import retrofit.client.OkClient;
+import retrofit.converter.GsonConverter;
 
 /**
  * Created by amgh on 18/02/15.
@@ -20,7 +22,7 @@ public class Generator {
         RestAdapter.Builder builder = new RestAdapter.Builder()
                 .setClient(new OkClient(new OkHttpClient()))
                 .setEndpoint(BuildConfig.SERVER_URL)
-                //.setConverter(converter)
+                .setConverter(new GsonConverter(new Gson()))
                 //.setRequestInterceptor(requestInterceptor)
                 //.setErrorHandler(errorHandler)
                 .setLogLevel(RestAdapter.LogLevel.FULL).setLog(new AndroidLog("WL -#-"));
